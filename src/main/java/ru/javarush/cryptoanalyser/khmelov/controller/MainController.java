8336660af633de5db1abf7dbecb19c7b8a85f3b6
Khmelov <akhmelev@gmail.com>
2022-07-08 20:02:57 +0200

    add exception handler (just in MainController)

--- src/main/java/ru/javarush/cryptoanalyser/khmelov/controller/MainController.java
+++ src/main/java/ru/javarush/cryptoanalyser/khmelov/controller/MainController.java
@@ -2,10 +2,18 @@ package ru.javarush.cryptoanalyser.khmelov.controller;
 
 import ru.javarush.cryptoanalyser.khmelov.commands.Action;
 import ru.javarush.cryptoanalyser.khmelov.entity.Result;
+import ru.javarush.cryptoanalyser.khmelov.entity.ResultCode;
+import ru.javarush.cryptoanalyser.khmelov.exception.ApplicationException;
 
 public class MainController {
     public Result execute(String command, String[] parameters) {
-        Action action=Actions.find(command);
-        return action.execute(parameters);
+        try {
+            Action action=Actions.find(command);
+            return action.execute(parameters);
+        } catch (ApplicationException e){
+            //TODO log file for exceptions
+            return new Result(ResultCode.ERROR,e.getMessage());
+        }
+
     }
 }
